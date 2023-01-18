6f705de1dcac9f270f187e85b2a04316eb8896cb
PheveKim <61591883+PheveKim@users.noreply.github.com>
2023-01-18 15:43:38 +0900

    [Bronze V] Title: 나머지, Time: 208 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int A = scanner.nextInt();
+		int B = scanner.nextInt();
+		int C = scanner.nextInt();
+		
+		System.out.println((A+B)%C);
+		System.out.println(((A%C)+ (B%C))%C );
+		System.out.println((A*B)%C);
+		System.out.println(((A%C)*(B%C))%C);
+		
+		
+	}
+	
+}
