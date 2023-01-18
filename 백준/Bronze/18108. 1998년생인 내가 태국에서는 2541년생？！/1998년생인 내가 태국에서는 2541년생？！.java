42c54a7a64d4b0a40a5cc7ddb91938ba0c9f5329
PheveKim <61591883+PheveKim@users.noreply.github.com>
2023-01-18 15:33:47 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 208 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println(scanner.nextInt() - 543);
+		
+		
+	}
+	
+}
