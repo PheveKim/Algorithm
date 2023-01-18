5e84ccaefb7eb0e57f3ce38df974b73c3360d7a1
PheveKim <61591883+PheveKim@users.noreply.github.com>
2023-01-18 15:48:03 +0900

    [Bronze V] Title: 고양이, Time: 200 ms, Memory: 17384 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+		
+		
+	}
+	
+}
