a3cdd9de96a392f1eee4e12f7249af6a434515d6
PheveKim <61591883+PheveKim@users.noreply.github.com>
2023-01-18 15:49:57 +0900

    [Bronze V] Title: 새싹, Time: 196 ms, Memory: 17400 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+		
+		
+	}
+	
+}
