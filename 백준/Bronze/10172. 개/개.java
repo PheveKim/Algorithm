573ba704a86c43dfa6ea62947739223b734c4c65
PheveKim <61591883+PheveKim@users.noreply.github.com>
2023-01-18 15:49:03 +0900

    [Bronze V] Title: 개, Time: 196 ms, Memory: 17444 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+		
+		
+	}
+	
+}
