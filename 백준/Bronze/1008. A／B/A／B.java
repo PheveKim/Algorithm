3afc43d7ddfa2557cf09e5dae1f3ad0085bb9e00
PheveKim <61591883+PheveKim@users.noreply.github.com>
2023-01-18 14:37:57 +0900

    [Bronze V] Title: A/B, Time: 220 ms, Memory: 18008 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		double A = scanner.nextDouble();
+		double B = scanner.nextDouble();
+		
+		
+		System.out.println(A/B);
+		
+	}
+	
+}
