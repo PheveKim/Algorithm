8287f14833f5aa86a0e263c234f189d06c178b2a
PheveKim <61591883+PheveKim@users.noreply.github.com>
2023-01-19 14:32:04 +0900

    [Bronze V] Title: 시험 성적, Time: 208 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int A = scanner.nextInt();
+		
+		if(A>=90 && A<=100) System.out.println("A");
+		else if (A>=80 && A<=89) System.out.println("B");
+		else if (A>=70 && A<=79) System.out.println("C");
+		else if (A>=60 && A<=69) System.out.println("D");
+		else System.out.println("F");
+		
+		
+		
+		
+	}
+	
+	
+}
