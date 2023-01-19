6ff86cfeb97a32cbcad09bacd7865e6e9fd5aee1
PheveKim <61591883+PheveKim@users.noreply.github.com>
2023-01-19 15:30:08 +0900

    [Bronze V] Title: A+B - 8, Time: 268 ms, Memory: 19000 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int T = scanner.nextInt();
+		
+		
+		for(int i=0; i<T; i++) {
+			int a = scanner.nextInt();
+			int b = scanner.nextInt();
+			
+			System.out.println("Case #"+(i+1)+": "+a+" + "+b+" = "+(a+b));
+		}
+		
+	}
+	
+}
