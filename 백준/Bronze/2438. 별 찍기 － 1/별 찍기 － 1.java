8203bd21a8365939a627e2a0a32252b229bceba5
PheveKim <61591883+PheveKim@users.noreply.github.com>
2023-01-19 15:44:22 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 212 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int N = scanner.nextInt();
+		String sum = "";
+		for(int i=0; i<N; i++) {
+			
+			sum = sum + "*";
+			System.out.println(sum);
+			
+		}
+		
+		
+		
+	}
+	
+}
