ca68c0aaa74e0fa20e99579e025715b1748510a4
PheveKim <61591883+PheveKim@users.noreply.github.com>
2023-01-21 19:46:41 +0900

    [Bronze V] Title: 아스키 코드, Time: 208 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		// Character.isDigit()
+		Scanner scanner = new Scanner(System.in);
+
+
+		char a = scanner.nextLine().charAt(0);
+		int num = (int)a;
+		System.out.println(num);
+		
+		//System.out.println(Character.isDigit(a));
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+}
