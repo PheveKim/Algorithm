04d453f8cbab2b6090f432b2c33e7c20a91b3ced
PheveKim <61591883+PheveKim@users.noreply.github.com>
2023-02-01 17:22:36 +0900

    [D3] Title: 삼성시의 버스 노선, Time: 327 ms, Memory: 89,940 KB -BaekjoonHub

--- SWEA/D3/6485. 삼성시의 버스 노선/삼성시의 버스 노선.java
+++ SWEA/D3/6485. 삼성시의 버스 노선/삼성시의 버스 노선.java
@@ -4,24 +4,28 @@ import java.lang.*;
 
 public class Solution {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 
-		Scanner scanner = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
 
-		int T = scanner.nextInt();
-		
+		int T = Integer.parseInt(br.readLine());
 		
 		
 		for(int i=0; i<T; i++) {
 			
 			HashMap<Integer, Integer> map = new HashMap<>();
 			
-			int N = scanner.nextInt();
+			int N = Integer.parseInt(br.readLine());
 			
 			for(int j=0; j<N; j++) {
 				
-				int A = scanner.nextInt();
-				int B = scanner.nextInt();
+				String str = br.readLine();
+				StringTokenizer st = new StringTokenizer(str, " ");
+				
+				int A = Integer.parseInt(st.nextToken());
+				int B = Integer.parseInt(st.nextToken());
 				
 				for(int k=A; k<B+1; k++) {
 					if(map.get(k) != null) map.put(k, map.get(k)+1);
@@ -30,15 +34,20 @@ public class Solution {
 			}
 			
 			
-			int P = scanner.nextInt();
+			int P = Integer.parseInt(br.readLine());
+			
 			System.out.print("#" + (i+1) + " ");
 			for(int j=0; j<P; j++) {
-				int C = scanner.nextInt();
+				int C = Integer.parseInt(br.readLine());
 				if(map.get(C) != null) System.out.print(map.get(C) + " ");
 				else System.out.print(0 + " ");
 			}
 			System.out.println();
 		}
+		
+		br.close();
+		bw.close();
+		
 		
 		
 		
