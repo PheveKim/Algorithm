98987e76e7af0af760d1e42f1b6d57c964a68bbc
PheveKim <61591883+PheveKim@users.noreply.github.com>
2023-02-13 14:34:25 +0900

    [D2] Title: 중간 평균값 구하기, Time: 118 ms, Memory: 17,148 KB -BaekjoonHub

--- SWEA/D2/1984. 중간 평균값 구하기/중간 평균값 구하기.java
+++ SWEA/D2/1984. 중간 평균값 구하기/중간 평균값 구하기.java
@@ -8,25 +8,25 @@ public class Solution {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
 		int T = Integer.parseInt(br.readLine());
 
 		for (int t = 0; t < T; t++) {
 
-			StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
-			int st1_length = st1.countTokens(); // countTokens() 함수를 for문 안에 넣으면 에러나더군요
-												// 먼저 정의해주고 사용해주어야하는듯...~~~
+			st = new StringTokenizer(br.readLine());
+			int max = 0;
 			ArrayList<Integer> list = new ArrayList<>();
-			for(int i=0; i<st1_length; i++) {
-				list.add(Integer.parseInt(st1.nextToken()));
+			for (int i = 0; i < 10; i++)
+				list.add(Integer.parseInt(st.nextToken()));
+			
+			double sum = 0;
+			Collections.sort(list);
+			for(int i=1; i<list.size()-1; i++) {
+				sum += list.get(i);
 			}
 			
-			Collections.sort(list);
-			list.remove(0);
-			list.remove(list.size()-1);
-			double sum = 0;
-			for(int i=0; i<list.size(); i++)
-				sum += list.get(i);
-			bw.write("#" + (t + 1) + " " + Math.round(sum/list.size()));
+			
+			bw.write("#" + (t + 1) + " " + Math.round(sum/(list.size()-2)));
 			bw.newLine();
 			bw.flush();
 
