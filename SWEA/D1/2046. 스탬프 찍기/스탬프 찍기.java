e01f1b9d70e360857c2b21f4586806fb433a9245
PheveKim <61591883+PheveKim@users.noreply.github.com>
2023-02-23 16:06:16 +0900

    [D1] Title: 스탬프 찍기, Time: 105 ms, Memory: 18,404 KB -BaekjoonHub

---
+++ SWEA/D1/2046. 스탬프 찍기/스탬프 찍기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+import java.math.*;
+
+public class Solution {
+
+	static int[] dr = { -1, 1, 0, 0 };
+	static int[] dc = { 0, 0, -1, 1 };
+	static int min;
+	static int N;
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+
+		int num = Integer.parseInt(br.readLine());
+		
+		for(int i=0; i<num; i++) {
+			System.out.print("#");
+		}
+	}
+
+}
