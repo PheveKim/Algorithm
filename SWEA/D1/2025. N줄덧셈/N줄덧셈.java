8a167e992f0cd3aadb30ac399408425c4388d17d
PheveKim <61591883+PheveKim@users.noreply.github.com>
2023-02-23 15:58:22 +0900

    [D1] Title: N줄덧셈, Time: 112 ms, Memory: 18,932 KB -BaekjoonHub

---
+++ SWEA/D1/2025. N줄덧셈/N줄덧셈.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+import java.math.*;
+
+public class Solution {
+
+	static int[] dr = { -1, 1, 0, 0 };
+	static int[] dc = { 0, 0, -1, 1 };
+	static int min;
+	static int N;
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+
+		int num = Integer.parseInt(br.readLine());
+		
+		int sum = 0;
+		for(int i=1; i<num+1; i++) {
+			sum += i;
+		}
+		System.out.println(sum);
+		
+		
+	}
+
+}
---
+++ SWEA/D1/2025. N줄덧셈/README.md
@@ -0,0 +1,11 @@
+# [D1] N줄덧셈 - 2025 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5QFZtaAscDFAUq) 
+
+### 성능 요약
+
+메모리: 18,932 KB, 시간: 112 ms, 코드길이: 597 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
