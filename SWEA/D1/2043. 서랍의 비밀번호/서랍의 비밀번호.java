62eb6bd140e5768d1d25582127e452a7963d4585
PheveKim <61591883+PheveKim@users.noreply.github.com>
2023-02-23 16:05:00 +0900

    [D1] Title: 서랍의 비밀번호, Time: 102 ms, Memory: 18,632 KB -BaekjoonHub

---
+++ SWEA/D1/2043. 서랍의 비밀번호/서랍의 비밀번호.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+import java.math.*;
+
+public class Solution {
+
+	static int[] dr = { -1, 1, 0, 0 };
+	static int[] dc = { 0, 0, -1, 1 };
+	static int min;
+	static int N;
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine());
+		
+		int P = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
+		
+		System.out.println((P-K+1));
+
+	}
+
+}
