9402c4bf599566160fc95fd5633e593155fe42f2
PheveKim <61591883+PheveKim@users.noreply.github.com>
2023-02-23 15:56:53 +0900

    [D1] Title: 더블더블, Time: 106 ms, Memory: 18,496 KB -BaekjoonHub

---
+++ SWEA/D1/2019. 더블더블/README.md
@@ -0,0 +1,11 @@
+# [D1] 더블더블 - 2019 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5QDEX6AqwDFAUq) 
+
+### 성능 요약
+
+메모리: 18,496 KB, 시간: 106 ms, 코드길이: 602 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D1/2019. 더블더블/더블더블.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+import java.math.*;
+
+public class Solution {
+
+	static int[] dr = { -1, 1, 0, 0 };
+	static int[] dc = { 0, 0, -1, 1 };
+	static int min;
+	static int N;
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+
+		int num = Integer.parseInt(br.readLine());
+		
+		int mul = 1;
+		for(int i=1; i<num+2; i++) {
+			System.out.print(mul + " ");
+			mul *= 2;
+		}
+		
+		
+	}
+
+}
