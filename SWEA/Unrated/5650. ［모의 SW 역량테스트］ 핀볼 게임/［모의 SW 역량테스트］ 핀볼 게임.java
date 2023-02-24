d4e48e113ee266f0aa18d6f939cc7f1ee9455049
PheveKim <61591883+PheveKim@users.noreply.github.com>
2023-02-25 02:16:27 +0900

    [Unrated] Title: [모의 SW 역량테스트] 핀볼 게임, Time: 788 ms, Memory: 41,340 KB -BaekjoonHub

--- SWEA/Unrated/5650. ［모의 SW 역량테스트］ 핀볼 게임/［모의 SW 역량테스트］ 핀볼 게임.java
+++ SWEA/Unrated/5650. ［모의 SW 역량테스트］ 핀볼 게임/［모의 SW 역량테스트］ 핀볼 게임.java
@@ -2,9 +2,9 @@ import java.util.*;
 import java.io.*;
 import java.lang.*;
 import java.math.*;
- 
+  
 public class Solution {
- 
+  
     static int[] dr = { -1, 1, 0, 0 };
     static int[] dc = { 0, 0, -1, 1 };
     static int max;
@@ -27,20 +27,16 @@ public class Solution {
     static int nr;
     static int nc;
     static String direction;
-    
-    
-    
+     
     public static void main(String[] args) throws IOException {
- 
-    	Scanner scanner = new Scanner(System.in);
+  
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st;
- 
-        int T = scanner.nextInt();
+  
+        int T = Integer.parseInt(br.readLine().trim());
         for (int t = 0; t < T; t++) {
- 
-            N = scanner.nextInt();
+            N = Integer.parseInt(br.readLine().trim());
             arr = new int[N][N];
             start_r.clear();
             start_c.clear();
@@ -48,15 +44,15 @@ public class Solution {
             Arrays.fill(wh_1_c, -1);
             Arrays.fill(wh_2_r, -1);
             Arrays.fill(wh_2_c, -1);
-             
-             
+            
             for (int row = 0; row < N; row++) {
+            	st = new StringTokenizer(br.readLine(), " ");
                 for (int col = 0; col < N; col++) {
-                    int num = scanner.nextInt();
+                    int num = Integer.parseInt(st.nextToken());
                     arr[row][col] = num;
                     if(num == 0) {
-                    	start_r.add(row);
-                    	start_c.add(col);
+                        start_r.add(row);
+                        start_c.add(col);
                     }
                     if(num >= 6 && num <= 10) {
                         int idx = num%6;
@@ -67,24 +63,20 @@ public class Solution {
                         else {
                             wh_2_r[idx] = row;
                             wh_2_c[idx] = col;
-                        }
-                                             
+                        }            
                     }
                 }
             }
- 
+  
             max = 0;
             for(int i=0; i<start_r.size(); i++) {
-            	pinball(arr, start_r.get(i), start_c.get(i));
+                pinball(arr, start_r.get(i), start_c.get(i));
             }
- 
             System.out.println("#" + (t + 1) + " " + max);
-            
         }
     }
- 
+  
     public static void pinball(int[][] arr, int row, int col) {
-    	
         for(int i=0; i<4; i++) {
             direction = D[i];
             k = 1;
@@ -94,7 +86,7 @@ public class Solution {
             direction_changed = false;
             r_dir = dr[i];
             c_dir = dc[i];
-             
+            
             while(true) {
                 if(direction_changed) {
                     for(int j=0; j<4; j++) {
@@ -104,37 +96,32 @@ public class Solution {
                         }
                     }
                 }
-                 
                 nr = next_row + r_dir * k;
                 nc = next_col + c_dir * k;
-                 
+                
                 if(nr<0 || nr>=N || nc<0 || nc>=N) {
-                	point = point * 2 + 1;
-                	max = Math.max(max, point);
-                	break;
+                    point = point * 2 + 1;
+                    max = Math.max(max, point);
+                    break;
                 }
-                 
+                  
                 else {
-                	
-                	if(arr[nr][nc] == -1) {
+                    if(arr[nr][nc] == -1) {
                         // 다음 위치에 블랙홀 발견
                         max = Math.max(max, point);
                         break;
                     }
-                	
                     else if(nr == row && nc == col) {
                         // 다음 위치가 처음 출발 위치
                         max = Math.max(max, point);
                         break;
                     }
-                	
-                	else if(arr[nr][nc] == 0) {
+                    else if(arr[nr][nc] == 0) {
                          // 그냥 길일때는 방향으로 1칸 전진
                          direction_changed = false;
                          k++;
                     }
-                     
-                	else if(arr[nr][nc] == 1) {
+                    else if(arr[nr][nc] == 1) {
                         point++;
                         next_row = nr;
                         next_col = nc;
@@ -144,7 +131,6 @@ public class Solution {
                         else if(direction.equals("left")) direction = "up";
                         else if(direction.equals("up")) direction = "down";
                         else if(direction.equals("down")) direction = "right";
-                         
                     }
                     else if(arr[nr][nc] == 2) {
                         point++;
@@ -186,7 +172,7 @@ public class Solution {
                     }
                     else if(arr[nr][nc] >= 6 && arr[nr][nc] <= 10) {
                         // 다음 위치에 웜홀 발견
-                    	int worm_num = arr[nr][nc]%6;
+                        int worm_num = arr[nr][nc]%6;
                         if(wh_1_r[worm_num] == nr && wh_1_c[worm_num] == nc) {
                             next_row = wh_2_r[worm_num];
                             next_col = wh_2_c[worm_num];
@@ -196,16 +182,9 @@ public class Solution {
                             next_col = wh_1_c[worm_num];
                         }
                         k = 1;
-                         
                     }
-                	
                 }
-                
             }
-            
         }
-         
-         
     }
- 
 }
