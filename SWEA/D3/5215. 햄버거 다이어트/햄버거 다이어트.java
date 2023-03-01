1b9b8beb6923d1cf9e5ac82868f3c8a964be8721
PheveKim <61591883+PheveKim@users.noreply.github.com>
2023-03-01 17:35:53 +0900

    [D3] Title: 햄버거 다이어트, Time: 195 ms, Memory: 20,232 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -1,55 +1,52 @@
 import java.util.*;
 import java.io.*;
-import java.lang.*;
+import java.math.*;
 
 public class Solution {
-
-	static int N, L, max_point;
-	static int[][] all;
-
+	static int N;
+	static int C;
+	static int max;
+	static int o;
 	public static void main(String[] args) throws IOException {
-
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+
 		int T = Integer.parseInt(br.readLine());
 
 		for (int t = 0; t < T; t++) {
-
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			st = new StringTokenizer(br.readLine(), " ");
 			N = Integer.parseInt(st.nextToken());
-			L = Integer.parseInt(st.nextToken());
-
-			all = new int[N][2];
+			C = Integer.parseInt(st.nextToken());
+			max = 0;
+			int[] tastes = new int[N];
+			int[] cals = new int[N];
+			boolean[] visited = new boolean[N];
 
 			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine(), " ");
-				all[i][0] = Integer.parseInt(st.nextToken());
-				all[i][1] = Integer.parseInt(st.nextToken());
+				tastes[i] = Integer.parseInt(st.nextToken());
+				cals[i] = Integer.parseInt(st.nextToken());
 			}
 
-			max_point = 0;
-			Select(0, 0, 0);
+			choose(tastes, cals, visited, 0, 0, 0);
 
-			bw.write("#" + (t + 1) + " " + max_point);
+			bw.write("#" + (t + 1) + " " + max);
 			bw.newLine();
 			bw.flush();
-
 		}
 
 	}
 
-	public static void Select(int idx, int point, int cal) {
-
-		if (cal > L)
-			return;
-		if (cal <= L)
-			max_point = Math.max(max_point, point);
-		if (idx == N)
-			return;
-
-		Select(idx + 1, point + all[idx][0], cal + all[idx][1]); // 사용함
-		Select(idx + 1, point, cal); // 사용안함
-
+	public static void choose(int[] tastes, int[] cals, boolean[] visited, int cal_sum, int taste_sum, int last_idx) {
+		for (int i = last_idx; i < N; i++) {
+			if (visited[i] == false && cal_sum + cals[i] <= C) {
+				max = Math.max(max, taste_sum + tastes[i]);
+				visited[i] = true;
+//				System.out.println(i + " " + (taste_sum + tastes[i]) + " " + (cal_sum + cals[i]) + "  " + Arrays.toString(visited));
+				choose(tastes, cals, visited, cal_sum + cals[i], taste_sum + tastes[i], i);
+				visited[i] = false;
+			}
+		}
 	}
-
 }
