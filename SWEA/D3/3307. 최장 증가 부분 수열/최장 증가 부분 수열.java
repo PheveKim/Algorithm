6666bbac71c8956e95f5a9f7aac6b5bc0184ff63
PheveKim <61591883+PheveKim@users.noreply.github.com>
2023-03-01 19:15:37 +0900

    [D3] Title: 최장 증가 부분 수열, Time: 154 ms, Memory: 27,628 KB -BaekjoonHub

--- SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
+++ SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.java
@@ -1,42 +1,53 @@
 import java.util.*;
 import java.io.*;
-import java.lang.*;
+import java.math.*;
 
 public class Solution {
-
-	public static void main(String[] args) throws IOException {
-
+	static int[] arr;
+	static int N;
+	static int max_length;
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		int T = Integer.parseInt(br.readLine());
-
-		for (int t = 0; t < T; t++) {
-
-			int N = Integer.parseInt(br.readLine());
-			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-			int[] arr = new int[N];
-			int[] dp = new int[N];
-
-			for (int i = 0; i < N; i++) {
+		StringTokenizer st;
+		
+		int T = Integer.parseInt(br.readLine().trim());
+		for(int t=0; t<T; t++) {
+			
+			N = Integer.parseInt(br.readLine().trim());
+			arr = new int[N];
+			st = new StringTokenizer(br.readLine(), " ");
+			
+			for(int i=0; i<N; i++) {
 				arr[i] = Integer.parseInt(st.nextToken());
-				dp[i] = 1;
 			}
-
-			int max = 0;
-			for (int i = 0; i < N; i++) {
-				for (int j = 0; j < i; j++) {
-					if (arr[j] <= arr[i])
+			
+			max_length = 0;
+			int[] dp = new int[N];
+			for(int i=0; i<N; i++) {
+				for(int j=0; j<i; j++) {
+					if(arr[j] <= arr[i]) {
 						dp[i] = Math.max(dp[i], dp[j] + 1);
+					}
 				}
-				max = Math.max(max, dp[i]);
+				max_length = Math.max(max_length, dp[i]);
 			}
-
-			bw.write("#" + (t + 1) + " " + max);
+			
+			bw.write("#" + (t+1) + " " + (max_length+1));
 			bw.newLine();
 			bw.flush();
-
 		}
-
 	}
-
 }
+
+	
+//	public static void choose(int last_idx, int last_num, int length) {
+//		System.out.println(last_idx + " " + last_num + " " + length);
+//		for(int i=last_idx+1; i<N; i++) {
+//			if(arr[i] >= last_num) {
+//				max_length = Math.max(max_length, length+1);
+//				choose(i, arr[i], length+1);
+//			}
+//		}
+//	}
+//}
