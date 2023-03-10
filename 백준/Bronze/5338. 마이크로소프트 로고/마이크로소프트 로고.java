ee0fbdad51d2c52aec1e70ea0f07bfa9318de6d4
PheveKim <61591883+PheveKim@users.noreply.github.com>
2023-03-10 16:13:15 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 72 ms, Memory: 11332 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+//		st = new StringTokenizer(br.readLine(), " ");
+//		int N = Integer.parseInt(br.readLine());
+//		int st_len = st.countTokens();
+//		String input = br.readLine();
+//		for(int i=0; i<N; i++) {
+//			System.out.println(i+1);
+//		}
+//	       _.-;;-._
+//	       '-..-'|   ||   |
+//	       '-..-'|_.-;;-._|
+//	       '-..-'|   ||   |
+//	       '-..-'|_.-''-._|
+	    System.out.println("       _.-;;-._");
+	    System.out.println("'-..-'|   ||   |");
+	    System.out.println("'-..-'|_.-;;-._|");
+	    System.out.println("'-..-'|   ||   |");
+	    System.out.println("'-..-'|_.-''-._|");
+	   
+	}
+}
