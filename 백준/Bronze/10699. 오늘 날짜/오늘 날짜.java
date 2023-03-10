3f6a2bedbc12b9489e0828456d8886000871768d
PheveKim <61591883+PheveKim@users.noreply.github.com>
2023-03-10 16:16:39 +0900

    [Bronze V] Title: 오늘 날짜, Time: 72 ms, Memory: 11328 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+//		st = new StringTokenizer(br.readLine(), " ");
+//		int N = Integer.parseInt(br.readLine());
+//		int st_len = st.countTokens();
+//		String input = br.readLine();
+//		for(int i=0; i<N; i++) {
+//			System.out.println(i+1);
+//		}
+//	       _.-;;-._
+//	       '-..-'|   ||   |
+//	       '-..-'|_.-;;-._|
+//	       '-..-'|   ||   |
+//	       '-..-'|_.-''-._|
+		System.out.println("2023-03-10");
+	   
+	}
+}
