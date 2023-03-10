009d0b45fcd3b1eaed81fcdfcdb500423da22965
PheveKim <61591883+PheveKim@users.noreply.github.com>
2023-03-10 16:10:39 +0900

    [Bronze V] Title: 등록, Time: 72 ms, Memory: 11324 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+//		st = new StringTokenizer(br.readLine(), " ");
+//		int N = Integer.parseInt(br.readLine());
+//		int st_len = st.countTokens();
+//		String input = br.readLine();
+//		for(int i=0; i<N; i++) {
+//			System.out.println(i+1);
+//		}
+		System.out.println(109);
+		System.out.println("tjdrb9999");
+	}
+}
