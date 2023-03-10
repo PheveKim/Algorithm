ef034ce347e0f624a7f9db2dabd5407cdae66754
PheveKim <61591883+PheveKim@users.noreply.github.com>
2023-03-10 15:49:08 +0900

    [Bronze V] Title: N 찍기, Time: 1420 ms, Memory: 31556 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+//		st = new StringTokenizer(br.readLine(), " ");
+		int N = Integer.parseInt(br.readLine());
+//		int st_len = st.countTokens();
+		
+		
+		for(int i=0; i<N; i++) {
+			System.out.println(i+1);
+		}
+		
+		
+	}
+}
