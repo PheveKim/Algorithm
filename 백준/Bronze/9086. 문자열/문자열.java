ecba651704ba69309a159cb599ab0798f3423d03
PheveKim <61591883+PheveKim@users.noreply.github.com>
2023-03-10 16:15:13 +0900

    [Bronze V] Title: 문자열, Time: 72 ms, Memory: 11448 KB -BaekjoonHub

--- 백준/Bronze/9086. 문자열/문자열.java
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -1,19 +1,32 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
+import java.math.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
 		
-		int T = sc.nextInt();
-		sc.nextLine();
-		for(int i=0; i<T; i++) {
-			String[] str = sc.nextLine().split("");
-			if(str.length>1) {
-				System.out.println(str[0]+str[str.length-1]);
-			}else {
-				System.out.println(str[0]+str[0]);
-			}
+//		st = new StringTokenizer(br.readLine(), " ");
+//		int N = Integer.parseInt(br.readLine());
+//		int st_len = st.countTokens();
+//		String input = br.readLine();
+//		for(int i=0; i<N; i++) {
+//			System.out.println(i+1);
+//		}
+//	       _.-;;-._
+//	       '-..-'|   ||   |
+//	       '-..-'|_.-;;-._|
+//	       '-..-'|   ||   |
+//	       '-..-'|_.-''-._|
+		int T = Integer.parseInt(br.readLine());
+		for(int t=0; t<T; t++) {
+			String str = br.readLine();
+			System.out.print(str.substring(0,1));
+			System.out.println(str.substring(str.length()-1,str.length()));
 			
 		}
+	   
 	}
 }
