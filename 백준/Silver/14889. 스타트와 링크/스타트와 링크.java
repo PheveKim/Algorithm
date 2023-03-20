79d11eb8f0b78b1ff42204a180bf398748ceb111
PheveKim <61591883+PheveKim@users.noreply.github.com>
2023-03-20 15:40:49 +0900

    [Silver II] Title: 스타트와 링크, Time: 264 ms, Memory: 25900 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -3,15 +3,11 @@ import java.io.*;
 import java.math.*;
 
 public class Main {
-	static boolean[] visited;
 	static int[][] arr;
 	static int n;
-	static int m;
-	static int cnt;
-	static int max;
 	static int min;
-	static int sum_all;
 	static boolean stop_find;
+	static boolean[] visited;
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,55 +23,44 @@ public class Main {
 			}
 		}
 		
-		sum_all = 0;
-		for(int i=0; i<arr.length; i++) {
-			for(int j=0; j<arr.length; j++) {
-				if(i != j) {
-					sum_all += arr[i][j];
-				}
-			}
-		}
 		min = Integer.MAX_VALUE;
 		stop_find = false;
+		visited = new boolean[n];
 		dfs(0, 0, new int[n/2]);
 		System.out.println(min);
 	}
 	
 	public static void dfs(int cnt, int last_idx, int[] chosen) {
-		
 		if(cnt >= chosen.length && stop_find == false) {
+			int idx = 0;
+			int[] chosen_2 = new int[n/2];
+			for(int i=0; i<n; i++) {
+				if(visited[i] == false) {
+					chosen_2[idx] = i;
+					idx++;
+				}
+			}
 			int sum_chosen = 0;
+			int sum_chosen_2 = 0;
 			for(int i=0; i<chosen.length; i++) { // 1, 2, 3
 				for(int j=0; j<chosen.length; j++) {
 					if(i != j) {
 						sum_chosen += arr[chosen[i]][chosen[j]];
+						sum_chosen_2 += arr[chosen_2[i]][chosen_2[j]];
 					}
 				}
 			}
-			ArrayList<Integer> list = new ArrayList<>();
-			for(int i=0; i<n; i++) {
-				list.add(i);
-			}
-			for(int i=0; i<chosen.length; i++) {
-				list.set(chosen[i], -1);
-			}
-			int sum_list = 0;
-			for(int i=0; i<list.size(); i++) {
-				for(int j=0; j<list.size(); j++) {
-					if(i != j && list.get(i) >= 0 && list.get(j) >= 0) {
-						sum_list += arr[i][j];
-					}
-				}
-			}
-			min = Math.min(min, Math.abs(sum_list - sum_chosen));
+			min = Math.min(min, Math.abs(sum_chosen_2 - sum_chosen));
 			if(min == 0) stop_find = true;
 		}
 		else {
 			for(int i=last_idx; i<n; i++) {
 				if(stop_find == false) {
 					chosen[cnt] = i;
+					visited[i] = true; // visited 배열을 주어 반복된 연산을 줄여주어 메모리와 시간을 절약한다!
 					dfs(cnt+1, i+1, chosen);
 					chosen[cnt] = 0;
+					visited[i] = false;
 				}
 			}
 		}
