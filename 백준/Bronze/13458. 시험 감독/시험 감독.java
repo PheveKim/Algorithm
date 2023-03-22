7dd0aa1188ba71345c9f1df3da58ad644c4f9671
PheveKim <61591883+PheveKim@users.noreply.github.com>
2023-03-22 17:55:34 +0900

    [Bronze II] Title: 시험 감독, Time: 432 ms, Memory: 125624 KB -BaekjoonHub

---
+++ 백준/Bronze/13458. 시험 감독/시험 감독.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		
+		int n = Integer.parseInt(br.readLine());
+		long[] arr = new long[n];
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		st = new StringTokenizer(br.readLine());
+		long b = Integer.parseInt(st.nextToken());
+		long c = Integer.parseInt(st.nextToken());
+		
+		long all_cnt = 0;
+//		if(b<c) {
+//			for(int i=0; i<n; i++) {
+//				int up = arr[i] / c;
+//				int left = arr[i] % c;
+//				if(left > 0) up++;
+//				all_cnt += up;
+//			}
+//		}
+//		else {
+		for(int i=0; i<n; i++) {
+			arr[i] -= b;
+			all_cnt++;
+			if(arr[i] > 0) {
+				long up = arr[i] / c;
+				long left = arr[i] % c;
+				if(left > 0) up++;
+				all_cnt += up;
+			}
+		}
+//		}
+		
+		System.out.println(all_cnt);
+		
+	}
+}
