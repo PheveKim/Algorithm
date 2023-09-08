fec3cf4c5ba3f62f0ace65982ef6903d8e1e34ce
PheveKim <61591883+PheveKim@users.noreply.github.com>
2023-09-09 00:58:48 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.25 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/lv2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -1,102 +1,87 @@
 import java.util.*;
-import java.math.*;
-import java.io.*;
-import java.lang.*;
 
 class Solution {
     public int[] solution(int[] fees, String[] records) {
-        StringTokenizer st;
-        
         int[] answer = {};
-        int basic_min = fees[0];
-        int basic_fee = fees[1];
-        int base = fees[2];
-        int over_fee = fees[3];
+        // 입차 -> 출차 : 차이 계산.
+        // 다시 입차 -> 출차 : 차이 계산.
         
-        HashMap<Integer, ArrayList<String[]>> map = new HashMap<>(); 
+        // 입차되어있는 차가 있는지 여부 해시맵.
+        // 입차 시간 해시맵.
+        // 입차 get 했는데 != null 이면 출차한것이므로 시간 차이 계산하고
+        // null 로 바꾸고, 입차여부해시맵을 false 로 변경.
         
+        HashMap<String, Integer> map_in = new HashMap<>();
+        HashMap<String, Boolean> isin = new HashMap<>();
+        HashMap<String, Integer> map_time = new HashMap<>();
+        
+        StringTokenizer st;
         for(int i=0; i<records.length; i++){
-            st = new StringTokenizer(records[i], " ");            
+            st = new StringTokenizer(records[i], " ");
             String time = st.nextToken();
-            int car_num = Integer.parseInt(st.nextToken());
-            String in_out = st.nextToken();
-            map.put(car_num, new ArrayList<String[]>());
-        }
-        
-        // "차 번호" : {시간, IN/OUT}
-        for(int i=0; i<records.length; i++){
-            st = new StringTokenizer(records[i], " ");            
-            String time = st.nextToken();
-            int car_num = Integer.parseInt(st.nextToken());
-            String in_out = st.nextToken();
+            String number = st.nextToken();
+            String inout = st.nextToken();
             
+            st = new StringTokenizer(time, ":");
+            int hour = Integer.parseInt(st.nextToken());
+            int minute = Integer.parseInt(st.nextToken());
+            int time_int = hour * 60 + minute;
             
-            map.get(car_num).add(new String[] {time, in_out});
-        }
-        
-        ArrayList<Integer> car_num_sorted = new ArrayList<>();
-        for(int key: map.keySet()){
-            car_num_sorted.add(key);
-        }
-        Collections.sort(car_num_sorted);
-        
-        answer = new int[map.size()];
-        int idx = 0;
-        for(int k=0; k<car_num_sorted.size(); k++){
-            int key = car_num_sorted.get(k);
-            int total_min_diff = 0;
-            int min_total_before = 0;
-            boolean in_only = true;
-            
-            for(int i=0; i<map.get(key).size(); i++){
-                String[] target = map.get(key).get(i);
-                String time = target[0];
-                String in_out = target[1];
-                
-                st = new StringTokenizer(time, ":");
-                int hour = Integer.parseInt(st.nextToken());
-                int min = Integer.parseInt(st.nextToken());
-                int min_total = hour * 60 + min;
-                
-                if(in_out.equals("IN")){
-                    min_total_before = min_total;
-                    in_only = true;
-                }
-                else if(in_out.equals("OUT")){
-                    in_only = false;
-                    int min_diff = min_total - min_total_before;
-                    total_min_diff += min_diff;
-                }
-                
+            if(inout.equals("IN")){
+                map_in.put(number, time_int);
+                isin.put(number, true);
             }
-            
-            if(in_only == true){
-                int last_min = 23*60 + 59;
-                int min_diff = last_min - min_total_before;
-                total_min_diff += min_diff;
+            else if (inout.equals("OUT")){
+                int before = map_in.get(number);
+                int time_diff = time_int - before;
+                
+                if(map_time.get(number) != null){
+                    map_time.put(number, map_time.get(number) + time_diff);       
+                }
+                else{
+                    map_time.put(number, time_diff);
+                }
+                isin.put(number, false);
             }
-            // System.out.println(total_min_diff);
-            int fee = 0;
-            if(total_min_diff <= basic_min){
-                fee = basic_fee;
+        }
+        
+        int last_time = 23 * 60 + 59;
+        for(String key: isin.keySet()){
+            if(isin.get(key) == true){
+                int time_diff = last_time - map_in.get(key);
+                if(map_time.get(key) != null){
+                    map_time.put(key, map_time.get(key) + time_diff);       
+                }
+                else{
+                    map_time.put(key, time_diff);
+                }
+            }
+        }
+        
+        HashMap<Integer, Integer> ans = new HashMap<>();
+        for(String key: map_time.keySet()){
+            int time = map_time.get(key);
+            if(time > fees[0]){
+                int remain = 0;
+                if((time - fees[0]) % fees[2] > 0) remain = 1;
+                ans.put(Integer.parseInt(key), fees[1] + ((time - fees[0]) / fees[2] + remain) * fees[3]);
             }
             else{
-                double diff_d = total_min_diff - basic_min;
-                double base_d = base;
-                
-                fee = basic_fee + 
-                    (int) Math.ceil(diff_d/base_d) * over_fee;
+                ans.put(Integer.parseInt(key), fees[1]);
             }
-            // System.out.println((total_min_diff - basic_min)/base);
-            // System.out.println(Math.ceil((total_min_diff - basic_min)/base));
-            answer[idx] = fee;
-            idx++;
         }
         
-        // 입차된 후에 출차된 내역이 없다면, 23:59에 출차된 것으로 간주합니다.
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int key: ans.keySet()){
+            list.add(key);
+        }
         
-        // 5000 + ⌈(334 - 180) / 10⌉ x 600 = 14600
-        // 초과한 시간이 단위 시간으로 나누어 떨어지지 않으면, 올림합니다.
-        return answer;
+        Collections.sort(list);
+        int[] result = new int[list.size()];
+        for(int i=0; i<list.size(); i++){
+            result[i] = ans.get(list.get(i));
+        }
+        
+        return result;
     }
 }
